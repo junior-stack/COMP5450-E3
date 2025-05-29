68f2aece16c26e30b098bcc096393782ba66d9e3
yezhou2 <jaxyz.ye@mail.utoronto.ca>
2025-05-29 00:07:35 -0400

    add start and reset button

--- lib/bucket_game.dart
+++ lib/bucket_game.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'package:demo/score_text.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
@@ -7,32 +6,60 @@ import 'package:flutter/material.dart';
 import 'bucket.dart';
 import 'ball.dart';
 
+enum GameState {menu, playing, gameOver}
+
 class BucketGame extends FlameGame with HasCollisionDetection, HasKeyboardHandlerComponents {
+  GameState gameState = GameState.menu;
+
   late Bucket bucket;
   late ScoreText scoreText;
   double spawnTimer = 0.0;
+
+  final List<Component> gameObjects = [];
 
   @override
   Color backgroundColor() => Colors.blue;
 
   @override
   Future<void> onLoad() async {
+    overlays.add('StartOverlay');
+  }
+
+  void startGame() {
+    gameState = GameState.playing;
+    overlays.remove('StartOverlay');
+    overlays.add('ResetButtonOverlay');
+
     bucket = Bucket();
     add(bucket);
 
-    // Add score text
     scoreText = ScoreText()
-    ..position = Vector2(size.x - 10, 10);
+      ..position = Vector2(size.x - 10, 10);
     add(scoreText);
+
+    gameObjects.addAll([bucket, scoreText]);
+  }
+
+  void resetGame() {
+    for(final obj in gameObjects){
+      obj.removeFromParent();
+    }
+    gameObjects.clear();
+    gameState = GameState.menu;
+    overlays.remove('GameOverOverlay');
+    overlays.add('StartOverlay');
   }
 
   @override
   void update(double dt) {
     super.update(dt);
-    spawnTimer += dt;
-    if (spawnTimer > 1.0) {
-      spawnTimer = 0.0;
-      add(Ball());
+    if(gameState == GameState.playing){
+      spawnTimer += dt;
+      if (spawnTimer > 1.0) {
+        spawnTimer = 0.0;
+        add(Ball());
+      }
+
     }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,18 @@ void main() {
   final game = BucketGame();
   runApp(Focus(
     autofocus: true,
-    child: GameWidget(game: game),
+    child: GameWidget(
+        game: game,
+        overlayBuilderMap: {
+            'StartOverlay': (context, game) => Center(
+              child: ElevatedButton(onPressed: () => (game as BucketGame).startGame(), child: const Text('Start Game')),
+            ),
+            'ResetButtonOverlay': (context, game) => Positioned(
+                top: 20,
+                left: 20,
+                child: ElevatedButton(onPressed: () => (game as BucketGame).resetGame(), child: const Text('Restart'))
+            )
+        },
+    ),
   ),);
 }
